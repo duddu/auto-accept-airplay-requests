37e0a90a7d9b3e41c2a609933f1c5ec49edfbb5c
Davide Doronzo <davide.doronzo@gmail.com>
2025-02-23 21:41:03 +0000

    chore: update build configuration

--- Accept AirPlay Requests/Package.swift
+++ Accept AirPlay Requests/Package.swift
@@ -1,17 +1,29 @@
 // swift-tools-version: 6.0
 
+import Foundation.NSURL
 import PackageDescription
 
+let productName: String = URL(filePath: #filePath)
+  .deletingLastPathComponent()
+  .lastPathComponent
+
 let package = Package(
-  name: "Accept AirPlay Requests Package",
-  platforms: [.macOS(.v13)],
+  name: "\(productName) Package",
+  platforms: [
+    .macOS(.v13)
+  ],
   targets: [
     .executableTarget(
-      name: "Accept AirPlay Requests"
+      name: productName
     ),
     .testTarget(
-      name: "Accept AirPlay Requests Tests",
-      dependencies: ["Accept AirPlay Requests"]
+      name: "\(productName) Tests",
+      dependencies: [
+        .byNameItem(
+          name: productName,
+          condition: .none
+        )
+      ]
     )
   ]
 )
